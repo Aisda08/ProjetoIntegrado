b84fbc36fdc12d682d7a42b5944dd9972cedd162
Aisda08 <gabrieldantas2003@outlook.com>
2025-08-10 09:07:58 -0300

    Redirecionar antes do login

--- src/static/js/redirecionar.js
+++ src/static/js/redirecionar.js
@@ -3,3 +3,9 @@ const port = window.location.port;
 function redirecionar(pag) {
     window.location.href = `http://${host}:${port}/${pag}`;
 }
+
+window.addEventListener('load', function() {
+    if (!sessionStorage.getItem('autenticado') && window.location.pathname != "/") {
+        redirecionar("")
+    }
+});
